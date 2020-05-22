c08bc7efd00f500aa5a5c1246e6355bc2ff096e9
bona0722 <bona0722@github.com>
2020-05-22 22:38:58 +0900

    part4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,16 +1,25 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'polls'
 
 urlpatterns = [ 
-    # 하드코딩한 것은 /polls/란 이름을 바꾸면 안 됨. 
-    path('', views.index, name='index'), #url을 만들 때 polls앱에서 polls/ < 라고 하면 인덱스 콜
-    #name이란 지금까지 url이 매칭돼왔다 polls하고 index매칭 . name이란것은 이때의 url을 우리가 이름으로 reference하려고 지은거다
-    #이 이름을 적으면 매칭돼 있는 url패턴 path부분을 가져올 수 있다. 그렇다면 index.html에서 "/polls/{{ question.id }}/" 이렇게 안 써도 된다.
-    path('<int:question_id>/', views.detail, name='detail'),#question id 주면 detail view를 보여줌
-    path('<int:question_id>/results/', views.results, name='results'),#question view를 주면 설문결과
-    path('<int:question_id>/vote/', views.vote, name='vote'), # voting하는걸 해주면 됨
-    #해주고 html도 만들어줘야함
+
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    #제너릭뷰를 사용하면 python코드를 작성하지 않아도 된다.
+    
+    # # 하드코딩한 것은 /polls/란 이름을 바꾸면 안 됨. 
+    # path('', views.index, name='index'), #url을 만들 때 polls앱에서 polls/ < 라고 하면 인덱스 콜
+    # #name이란 지금까지 url이 매칭돼왔다 polls하고 index매칭 . name이란것은 이때의 url을 우리가 이름으로 reference하려고 지은거다
+    # #이 이름을 적으면 매칭돼 있는 url패턴 path부분을 가져올 수 있다. 그렇다면 index.html에서 "/polls/{{ question.id }}/" 이렇게 안 써도 된다.
+    # path('<int:question_id>/', views.detail, name='detail'),#question id 주면 detail view를 보여줌
+    # path('<int:question_id>/results/', views.results, name='results'),#question view를 주면 설문결과
+    # path('<int:question_id>/vote/', views.vote, name='vote'), # voting하는걸 해주면 됨
+    # #해주고 html도 만들어줘야함
+    
 ]
 #나한테 위임한 게 있을텐데 아무것도 없으면 view.index를 불러달라.
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,27 +1,61 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
 #from django.template import loader
-from .models import Question
+from .models import Question, Choice
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template loading해서 거기에서 question object 역순으로 꺼내서 리스트로 만듬. 템플릿을 이용해서 
-    #template = loader.get_template('polls/index.html') #이거 rendeing함
-    context = {
-        'latest_question_list': latest_question_list, #이렇게 사용하는 게 있으면 이거다.
-    }
-    #return HttpResponse(template.render(context, request)) 
-    return render(request, 'polls/index.html', context) 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+        
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) 
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     #template loading해서 거기에서 question object 역순으로 꺼내서 리스트로 만듬. 템플릿을 이용해서 
+#     #template = loader.get_template('polls/index.html') #이거 rendeing함
+#     context = {
+#         'latest_question_list': latest_question_list, #이렇게 사용하는 게 있으면 이거다.
+#     }
+#     #return HttpResponse(template.render(context, request)) 
+#     return render(request, 'polls/index.html', context) 
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
